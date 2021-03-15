6394d874dd7615be1f65fed5d07a6099cf1c0a41
OlliMm <62538897+OlliMm@users.noreply.github.com>
2021-03-15 21:39:36 +0200

    Liikuta matoa, Mato ylipelitason, Mato syö omenan

--- mato.js
+++ mato.js
@@ -1,6 +1,10 @@
 var gridSize = (tileSize = 20); // 20 x 20 = 400
 var nextX = (nextY = 0);
 
+
+/* omena alustetaan ja
+käärmeen alkupituus on 3 blokkia ja käärme kasvaa kun se syö omenia*/
+
 // Mato
 var defaultTailSize = 3;
 var tailSize = defaultTailSize;
@@ -20,10 +24,39 @@ var appleX = (appleY = 15);
       }
 
       function Lopetus(x) {
-         // peliKäynnissä falseksi
+         // peliKäynnissä = falseksi
          clearInterval(pelinkontrolli);
          peliKäynnissä = false;
          document.getElementById("pelinstatus").innerHTML = "<small>Peli loppui!</small>";
          document.getElementById("pisteet").innerHTML = "<h1>Pisteet: " + x + "</h1>";
      }
-// Tein nämä t: olli
+
+    function draw() {
+        // Liikuta matoa
+       snakeX += nextX;
+       snakeY += nextY;
+
+        // Mato yli pelitason
+       if (snakeX < 0) {
+         snakeX = gridSize - 1;
+       }
+       if (snakeX > gridSize - 1) {
+         snakeX = 0;
+       }
+
+       if (snakeY < 0) {
+         snakeY = gridSize - 1;
+       }
+       if (snakeY > gridSize - 1) {
+         snakeY = 0;
+       }
+
+       // Mato syö omenan =
+       if (snakeX == appleX && snakeY == appleY) {
+         tailSize++;
+
+         appleX = Math.floor(Math.random() * gridSize);
+         appleY = Math.floor(Math.random() * gridSize);
+       }
+
+       // Voin tehä javascriptin t: olli
